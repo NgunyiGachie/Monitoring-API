827af85cecbb8785d84f61ae72fe4dfe6d4ea0af
NgunyiGachie <antogachie@gmail.com>
2025-01-29 01:57:53 +0300

    add user model

--- models/user.py
+++ models/user.py
@@ -1,3 +1,28 @@
 """
 Tracks user information and preferences for carbon offset calculations
 """
+from ..database import db
+from flask_bcrypt import Bcrypt
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+
+bcrypt = Bcrypt
+
+class User(db.Model):
+    __tablename__= "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash("utf-8")
