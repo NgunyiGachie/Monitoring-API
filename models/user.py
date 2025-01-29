993b381f2752c93c64ee0c91451fdf74e9980961
NgunyiGachie <antogachie@gmail.com>
2025-01-30 01:07:22 +0300

    update user model

--- models/user.py
+++ models/user.py
@@ -2,11 +2,11 @@
 Tracks user information and preferences for carbon offset calculations
 """
 from ..database import db
-from flask_bcrypt import Bcrypt
+from argon2 import PasswordHasher
+from argon2.exceptions import VerificationError
 from sqlalchemy.orm import validates
-from sqlalchemy.ext.hybrid import hybrid_property
 
-bcrypt = Bcrypt
+ph = PasswordHasher
 
 class User(db.Model):
     __tablename__= "users"
@@ -18,11 +18,48 @@ class User(db.Model):
     image_url = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-    @hybrid_property
+    @property
     def password_hash(self):
-        raise AttributeError("Password hashes may not be viewed")
+        raise AttributeError("Password hashes cannot be viewed")
 
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
-        self._password_hash = password_hash("utf-8")
+        self._password_hash = ph.hash(password)
+
+    def authenticate(self, password):
+        try:
+            ph.verify(self._password_hash, password)
+            return True
+        except VerificationError:
+            return False
+
+    @validates("username")
+    def validate_username(self, username):
+        if not username:
+            raise AssertionError("No username provided")
+        if User.query.filter(User.username == username).first():
+            raise AssertionError("Usernae is already in use")
+        if len(username) < 5 or len(username) > 20:
+            raise AssertionError("Username must be between 5 and 20 characters")
+        return username
+
+    @validates("email")
+    def validate_email(self, email):
+        if not email:
+            raise AssertionError("No email provided")
+        if User.query.filter(User.email == email).first():
+            raise AssertionError("Email is already in use")
+        if '@' not in email:
+            raise AssertionError("Invalid email")
+        return email
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'image_url': self.image_url,
+            'created_at': self.created_at,
+        }
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}>"
