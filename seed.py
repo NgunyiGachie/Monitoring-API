974a881c74adfcd322351dc9e22bf73602c65bb6
NgunyiGachie <antogachie@gmail.com>
2025-02-04 23:49:08 +0300

    seed students

--- seed.py
+++ seed.py
@@ -20,5 +20,22 @@ with app.app_context():
     db.create_all()
 
     users = [
-        
+        User(
+           username = 'antogachie',
+           email = 'myemail@gmail.com',
+           image_url = 'https://example.com/images/antogachie.jpg',
+           created_at = datetime(2025, 2, 1)
+        ),
+        User(
+            username = 'mrmorty',
+            email = 'morty@gmail.com',
+            image_url = 'https://example.com/images/morty.jpg',
+            created_at = datetime(2025, 2, 1)
+        )
     ]
+    for user, password in zip(users, ['password1', 'password2']):
+        user.password_hash = ph.hash(password)
+    print("adding users to the database....")
+    db.session.add_all(users)
+    db.session.commit()
+    print(f"{len(users)} users added")
